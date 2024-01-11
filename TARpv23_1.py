0d1f1ecf2abd4c9507318e788f551f7805104d41
opilane <opilane@HP-CZC2349HTP>
2024-01-11 12:14:40 +0200

    арыврыр

--- TARpv23_1.py
+++ TARpv23_1.py
@@ -5,6 +5,7 @@
 #print("Tere tulemast!",nimi,". Sa oled",vanus,"aastat vana") #если запятая то дальше все пишем через нее
 #print("Tere tulrmast! {0} Sa oled {1} aastat vana".format(nimi,vanus)) #меняется толлько то что в фигруных скобках
 #print("Muutuja vanus=",vanus,"tüüp on",type(vanus))
+
 #2
 #vanus = 18
 #eesnimi = "Jaak"
@@ -13,12 +14,35 @@
 #print("Muutuja vanus=",vanus,"tüüp on",type(vanus))
 #print("Muutuja eesnimi=",eesnimi,"tüüp on",type(eesnimi))
 #print("Muutuja pikkus=",pikkus,"tüüp on",type(pikkus))
-#print("Muutuja kas_käib_koolis=",kas_käib_koolis,"tüüp on",type(kas_käib_koolis))
-#3
-from random import *
-kokku=randint(10,100)
-print("kokku: ",kokku)
-mitu=int(input("Mitu kommi tahad võtta?"))
-kokku=mitu
-print("Nüüd laua peal on",kokku,"kommid")
-4
+##print("Muutuja kas_käib_koolis=",kas_käib_koolis,"tüüp on",type(kas_käib_koolis))
+
+##3
+#from random import *
+#kokku=randint(10,100)
+#print("kokku: ",kokku)
+#mitu=int(input("Mitu kommi tahad võtta?"))
+#kokku=mitu
+#print("Nüüd laua peal on",kokku,"kommid")
+
+##10
+#from random import 
+#P=randint(1,5)
+#hind=12.90
+#hind=1.1 #hind koos jäätatega
+#osa=round(hind/P,2)
+#print("Iga sõber maksab:  ",osa)
+
+##6
+#from sunau import AUDIO_FILE_ENCODING_ADPCM_G721, AUDIO_FILE_ENCODING_ADPCM_G722
+
+
+try:
+    aeg = float(input("Minu tundi kulus sõiduks? ")) #на ноль нельзя делить
+    treeppikus = float(input("Minu kilomeetrit sõitsid? ")) 
+    kiirus = treepikkus/aeg 
+    print("Sinu kiirus oli " + str(kiirus) + " km/h")
+
+except :
+    passprint("Viga andmetüübiga")
+
+    print()
